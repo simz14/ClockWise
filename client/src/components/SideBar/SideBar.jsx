e0cfd7668b3b5b8586ce07f6a96d9aabbcdfab14
Simona Zozuľaková <szozulakova@gmail.com>
2023-05-16 16:06:35 +0200

    sidebar skeleton

---
+++ client/src/components/SideBar/SideBar.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledBar = styled.div`
+  min-height: 100vh;
+`;
+
+const SideBar = () => {
+  return <StyledBar></StyledBar>;
+};
+
+export default SideBar;
